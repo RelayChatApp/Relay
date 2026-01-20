003ad05e7fa00d0a8135e2fa351275868a510ce7
Muskan0266 <25muskaaan@gmail.com>
2026-01-20 18:51:25 +0530

    validations

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,7 +1,70 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Signup = () => {
+    const [form, setForm] = useState({
+        FName: "",
+        password: "",
+        confirmPassword: "",
+        email: ""
+
+    })
+    const [err, setErr] = useState("")
+
+    function setFormValue(e) {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    function submit(e) {
+        e.preventDefault()
+
+        //Field constraints
+        if (!form.FName.trim() || !form.email.trim() || !form.password.trim() || !form.confirmPassword.trim()) {
+            setErr("All fields are required")
+            return
+        }
+
+        if (!form.email.includes("@")) {
+            setErr('Invalid Email')
+            return
+
+        }
+
+        //Password constraints
+
+        if (form.password.length < 8) {
+            setErr("Password too short")
+            return
+        }
+        if (!/[A-Z]/.test(form.password) || !/[@#$%]/.test(form.password) || !/[0-9]/.test(form.password)) {
+            setErr("Password not strong")
+            return
+        }
+        if (form.confirmPassword != form.password) {
+            setErr("Passwords don't match")
+            return
+        }
+        if (form.value == "") {
+            setErr("All fields are required")
+
+        }
+        setErr("")
+        setForm({
+            FName: "",
+            password: "",
+            confirmPassword: "",
+            email: ""
+
+        })
+    }
+
+
+
+
     return (
         <>
             <div className="bg-[#dbecff]  flex justify-center items-center">
@@ -29,21 +92,26 @@ const Signup = () => {
                         </Link>
                     </p>
                     <br />
-                    <form className=' w-screen'>
+
+                    <form onSubmit={submit} className=' w-screen'>
                         <div className='flex items-center justify-center'>
                             <input
                                 type="text"
-                                name="fullName"
+                                name="FName"
                                 placeholder='Full Name'
+                                value={form.FName}
+                                onChange={setFormValue}
                                 className=' p-4 w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-white text-[8E9AAF] font-bold'
                             />
                         </div>
                         <br />
                         <div className='flex items-center justify-center'>
                             <input
-                                type="email"
+                                type="text"
                                 name="email"
                                 placeholder='Email'
+                                value={form.email}
+                                onChange={setFormValue}
                                 className=' p-4 w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-white text-[8E9AAF] font-bold'
                             />
                         </div>
@@ -53,6 +121,8 @@ const Signup = () => {
                                 type="password"
                                 name="password"
                                 placeholder='Password'
+                                value={form.password}
+                                onChange={setFormValue}
                                 className=' p-4 w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-white text-[8E9AAF] font-bold '
 
                             />
@@ -63,15 +133,23 @@ const Signup = () => {
                                 type="password"
                                 name="confirmPassword"
                                 placeholder='Confirm Password'
+                                value={form.confirmPassword}
+                                onChange={setFormValue}
                                 className=' p-4 w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-white text-[8E9AAF] font-bold '
 
                             />
                         </div>
                         <br />
+                        <div className='flex items-center justify-center'>
+                            <p className='text-red-600 text-lg'>{err}</p>
+                        </div>
+
                         <div className='flex items-center justify-center '>
+
                             <button
+
                                 type='submit'
-                                className=' p-4 w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-blue-600  text-white  font-bold text-lg'
+                                className='p-4  w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-blue-600  text-white  font-bold text-lg'
                             >
                                 Create Account
                             </button>
