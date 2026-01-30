84e671c17eb5a45d7ad6ed240db34478523d8c18
kartik3030 <kartik.vij2004@gmail.com>
2026-01-30 19:35:48 +0530

    UI Update

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -21,19 +21,19 @@ const Login = () => {
     }
 
     return (<>
-        <div className='flex justify-center bg-[#dbecff] min-h-screen'>
+        <div className='flex justify-center bg-amber-100 min-h-screen'>
             <div>
-                <h1 className='font-extrabold text-5xl mt-5 text-center fascinate-regular'>Relay</h1>
+                <h1 className='font-extrabold text-5xl mt-5 text-center fascinate-regular text-amber-950'>Relay</h1>
 
                 {/* Logo */}
                 <div className='flex justify-center mt-5'>
                     <img
-                        src="https://cdn-icons-png.flaticon.com/512/6016/6016788.png"
+                        src="https://www.gloryofthesnow.com/wp-content/uploads/2022/08/Viber-Fall.png"
                         alt="Logo"
-                        className='w-40 h-40'
+                        className='w-40 h-40 rounded-[50%]'
                     />
                 </div>
-                <h3 className='mt-5 font-bold text-3xl text-center'>Welcome Back!!</h3>
+                <h3 className='mt-5 font-bold text-3xl text-center text-amber-950'>Welcome Back!!</h3>
                 <h4
                     className='mt-1 text-center text-gray-400'>Not a {" "}
                     <Link to="/signup">
@@ -73,7 +73,7 @@ const Login = () => {
                     <div className='flex items-center justify-center '>
                         <button
                             type='submit'
-                            className=' p-4 w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-blue-600  text-white  font-bold text-lg'
+                            className=' p-4 w-[90%] sm:w-100 rounded-[100px] shadow-2xl bg-amber-950  text-white  font-bold text-lg'
                         >
                             Login
                         </button>
