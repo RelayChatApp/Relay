45131c45b7ac406bdc42a92bad96c8957aa325cf
kartik3030 <kartik.vij2004@gmail.com>
2026-01-31 19:58:17 +0530

    Landing Page

---
+++ Frontend/src/Pages/Landing.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <>
+            <div className='flex justify-center bg-[#dbecff] min-h-screen'>
+                <div>
+                    <h1 className='font-extrabold text-5xl mt-5 text-center fascinate-regular'>Relay</h1>
+
+                    {/* Logo */}
+                    <div className='flex justify-center mt-5'>
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/6016/6016788.png"
+                            alt="Logo"
+                            className='w-40 h-40'
+                        />
+                    </div>
+
+
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Landing
