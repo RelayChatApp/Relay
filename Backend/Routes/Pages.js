d85ba1d6f563fce875a6ff2d314b7b0da91c641c
Muskan0266 <25muskaaan@gmail.com>
2026-02-12 19:58:28 +0530

    pages routes

---
+++ Backend/Routes/Pages.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../Schema/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+router.post("/profile", async (req, res) => {
+    const validUser = req.cookies.token
+    if (!validUser) {
+        res.status(401).json({ message: "User not authenticated" })
+    }
+    const verify = jwt.verify(token, process.env.JWT_SECRET)
+    const id = verify.id
+
+    const { email, FName } = req.body
+
+    const EmailCheck = User.findOne({ email })
+    if (EmailCheck) {
+        res.status(401).status({ message: "Email already in use" })
+    }
+
+    User.findByIdAndUpdate(id, { email, FName })
+
+})
